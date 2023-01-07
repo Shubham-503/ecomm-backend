80ce81d2ccadf9d61d36e6387b469096c6ffa03e
Shubham-503 <kr.shubham025@gmail.com>
2023-01-07 15:53:04 +0530

    Added user Model

---
+++ models/user.schema.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import crypto from "crypto"
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      maxLength: [50, "Name can't be more than 50 characters"]
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, 'Password must be atleast 8 characters'],
+      select: false
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true
+  }
+)
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password, 10);
+  next()
+})
+
+export default mongoose.model("User", userSchema)
